6df157e08c90cd3230e02a27ef90bd31263aa7f3
RaffaeleFiorillo <raffadeus8@gmail.com>
2022-11-28 21:45:45 -0100

    update

--- src/Game/HUD.py
+++ src/Game/HUD.py
@@ -1,6 +1,8 @@
 class HUD:
     def __init__(self, num):
-        self.number = num
+        self.number = num  # information if it is player one or player 2
+        self.life = 100
+        self.energy = 100
 
     def draw(self, screen):
         pass
